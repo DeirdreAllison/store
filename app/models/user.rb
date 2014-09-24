f8d67cafb5b88be15caa09813e2446d0039a8f00
DEIRDRE ALLISON <DEIRDRE9@GMAIL.com>
2014-09-24 16:53:11 -0700

    Fix params to work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,5 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
-  end
-
   validates_uniqueness_of :email
 end
